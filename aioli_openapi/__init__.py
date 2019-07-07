cf5bbafd6c7b84a805062f7215901896df38f353
Robert Wikman <rbw@vault13.org>
2019-07-07 20:23:53 +0200

    Add config schema

--- aioli_openapi/__init__.py
+++ aioli_openapi/__init__.py
@@ -1,7 +1,7 @@
 from aioli import Package
 from .controller import HttpController
 from .service import OpenApiService
-# from .config import ConfigSchema
+from .config import ConfigSchema
 
 export = Package(
     name="aioli_openapi",
@@ -9,4 +9,5 @@ export = Package(
     description="Generate OpenAPI Schemas using Aioli Controllers",
     controllers=[HttpController],
     services=[OpenApiService],
+    config=ConfigSchema
 )
