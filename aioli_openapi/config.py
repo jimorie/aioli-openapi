f70e0e9c5fe5fe15dc37c3ed823fc5ec5879228a
Robert Wikman <rbw@vault13.org>
2019-08-01 00:20:56 +0200

    Add path
    Signed-off-by: Robert Wikman <rbw@vault13.org>

--- aioli_openapi/config.py
+++ aioli_openapi/config.py
@@ -2,4 +2,5 @@ from aioli.config import PackageConfigSchema, fields
 
 
 class ConfigSchema(PackageConfigSchema):
+    path = fields.String(missing="/openapi")
     oas_version = fields.String(required=False, missing="3.0.2")
